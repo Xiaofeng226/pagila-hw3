b28e219062fea5fd57bd6f41df9b28821d5bc14d
Xiaofeng226 <xiaofengli878@gmail.com>
2026-03-24 21:13:28 -0700

    finished the sql questions

--- sql/14.sql
+++ sql/14.sql
@@ -7,3 +7,19 @@
  * Note that in the last query, we were ranking films by the total amount of payments made,
  * but in this query, you are ranking by the total number of times the movie has been rented (and ignoring the price).
  */
+
+
+
+SELECT c.name, f.title, f.rentals AS "total rentals"
+FROM category c
+JOIN LATERAL (
+    SELECT f.title, f.film_id, count(r.rental_id) AS rentals,
+           rank() OVER (ORDER BY count(r.rental_id) DESC, f.film_id DESC) AS rank
+    FROM film_category fc
+    JOIN film f USING (film_id)
+    JOIN inventory i USING (film_id)
+    JOIN rental r USING (inventory_id)
+    WHERE fc.category_id = c.category_id
+    GROUP BY f.film_id, f.title
+) AS f ON f.rank <= 5
+ORDER BY c.name, f.rentals DESC, f.film_id ASC;
